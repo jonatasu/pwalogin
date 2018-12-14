5f720227ded8956cdcd39977136a90a28d44bbea
JONATAS FILIPE VIEIRA <JonatasVieira@FRANCISCANOS.NET>
2018-12-14 16:07:27 -0200

    Vuetify added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
 
